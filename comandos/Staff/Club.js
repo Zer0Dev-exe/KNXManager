b5d7988a44aa85c39f2567e70c1914c0a5c73b53
Zer0Dev-exe <igorlizaso16@gmail.com>
2023-12-08 11:37:12 +0000

    KNX update

--- comandos/Staff/Club.js
+++ comandos/Staff/Club.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
 const BrawlStars = require("brawlstars.js")
 const token = process.env.APITOKEN
 const cliente = new BrawlStars.Client(token)
@@ -15,24 +15,110 @@ module.exports = {
     const club = await cliente.getClub('#CV8QU2VC')
     console.log(club)
     let presidente = club.members.find(member => member.role === 'president');
-    if(presidente) {
-      const nombrepresidente = presidente.name
-      const copaspresidente = presidente.trophies
+    let vicepresidente = club.members.find(member => member.role === 'vicePresident');
+        
+      const miembrosplus = club.members.sort((a, b) => b.trophies - a.trophies).slice(0, 5)
+      const miembrosminus = club.members.sort((a, b) => a.trophies - b.trophies).slice(0, 5)
 
-      const embed = new EmbedBuilder()
-      .setDescription(` \`\`\`${club.description}\`\`\` `)
-      .setThumbnail(`https://cdn-old.brawlify.com/club/${club.badgeId}.png`)
-      .setColor('Random')
-      .addFields(
-        { name: 'Trofeos Totales', value: `<:trophy:1178100595530420355> \`${club.trophies}\``, inline: true },
-        { name: 'Trofeos Requeridos', value: `<:reseteo:1178100588114882652> \`${club.requiredTrophies}\``, inline: true},
-        { name: 'Media Trofeos', value: `<:trophy:1178100595530420355> \`${Math.floor(club.trophies/club.memberCount)}\``, inline: true},
-        { name: 'Miembros', value: `<:BrawlStars:1001445217993498634> \`${club.memberCount}/30\``, inline: true },
-        { name: 'Estado', value: `<:1110136571400831037:1165754219203149915> \`${club.type}\``, inline: true },
-        { name: 'Presidente', value: `<a:BoosterC:1079712554785452032> \`${copaspresidente}\` **|** \`${nombrepresidente}\``, inline: true }
-      )
-      .setAuthor({ name: `${club.name} ${club.tag}`, iconURL: `https://cdn-old.brawlify.com/club/${club.badgeId}.png`})
-      interaction.reply({ embeds: [embed]})
-    }
+      // SI HAY PLAZAS
+      if(club.memberCount < 30) {
+        const boton = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+          .setLabel(`Estado: Abierto`)
+          .setCustomId('test1')
+          .setEmoji('957238576511340604')
+          .setStyle(ButtonStyle.Secondary)
+          .setDisabled(true)
+        )
+        const embed = new EmbedBuilder()
+        .setDescription(` \`\`\`${club.description}\`\`\` `)
+        .setThumbnail(`https://cdn-old.brawlify.com/club/${club.badgeId}.png`)
+        .setColor('#b96eff')
+        .addFields(
+          { name: 'Trofeos Totales', value: `<:trophy:1178100595530420355> \`${club.trophies}\``, inline: true },
+          { name: 'Trofeos Requeridos', value: `<:reseteo:1178100588114882652> \`${club.requiredTrophies}\``, inline: true},
+          { name: 'Media Trofeos', value: `<:trophy:1178100595530420355> \`${Math.floor(club.trophies/club.memberCount)}\``, inline: true},
+          { name: 'Miembros', value: `<:BrawlStars:1001445217993498634> \`${club.memberCount}/30\``, inline: true },
+          { name: 'Estado', value: `<:1110136571400831037:1165754219203149915> \`${club.type}\``, inline: true },
+          { name: 'Presidente', value: `<:StaffWarnIcon:1074154030173003826> \`${presidente.trophies}\` **|** \`${presidente.name}\``, inline: true },
+          { name: 'VicePresidente', value: `<:Utilidad:1074137802620551321> \`${vicepresidente.trophies}\` **|** \`${vicepresidente.name}\``, inline: true },
+          { name: ' ', value: ' ', inline: false }
+        )
+        .setAuthor({ name: `${club.name} ${club.tag}`, iconURL: `https://cdn-old.brawlify.com/club/${club.badgeId}.png`})
+
+        const membersField = miembrosplus.map((member) => {
+          const { name, trophies } = member
+          return `<:reseteo:1178100588114882652> \`${trophies}\` | \`${name}\``
+        }).join('\n')
+
+        const membersField2 = miembrosminus.map((member) => {
+          const { name, trophies } = member
+          return `<:reseteo:1178100588114882652> \`${trophies}\` | \`${name}\``
+        }).join('\n')
+    
+        embed.addFields(
+          { name: 'Miembros Altos', value: `${membersField}`, inline: true },
+          { name: 'Miembros Bajos', value: `${membersField2}`, inline: true }
+        )
+
+        interaction.reply({ embeds: [embed], components: [boton]})
+      } 
+
+      // SI ESTA LLENO
+      if(club.memberCount === 30) {
+
+        const boton = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+          .setLabel(`Estado: Lleno`)
+          .setCustomId('test1')
+          .setEmoji('957238576528105482')
+          .setStyle(ButtonStyle.Secondary)
+          .setDisabled(true)
+        )
+        const boton2 = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+          .setLabel(`Miembros Club`)
+          .setCustomId('lista-miembros')
+          .setEmoji('907339654850809916')
+          .setStyle(ButtonStyle.Success)
+        )
+        const embed = new EmbedBuilder()
+        .setDescription(` \`\`\`${club.description}\`\`\` `)
+        .setThumbnail(`https://cdn-old.brawlify.com/club/${club.badgeId}.png`)
+        .setColor('#b96eff')
+        .addFields(
+          { name: 'Trofeos Totales', value: `<:trophy:1178100595530420355> \`${club.trophies}\``, inline: true },
+          { name: 'Trofeos Requeridos', value: `<:reseteo:1178100588114882652> \`${club.requiredTrophies}\``, inline: true},
+          { name: 'Media Trofeos', value: `<:trophy:1178100595530420355> \`${Math.floor(club.trophies/club.memberCount)}\``, inline: true},
+          { name: 'Miembros', value: `<:BrawlStars:1001445217993498634> \`${club.memberCount}/30\``, inline: true },
+          { name: 'Estado', value: `<:1110136571400831037:1165754219203149915> \`${club.type}\``, inline: true },
+          { name: 'Presidente', value: `<:StaffWarnIcon:1074154030173003826> \`${presidente.trophies}\` **|** \`${presidente.name}\``, inline: true },
+          { name: 'VicePresidente', value: `<:Utilidad:1074137802620551321> \`${vicepresidente.trophies}\` **|** \`${vicepresidente.name}\``, inline: true },
+          { name: ' ', value: ' ', inline: false }
+        )
+        .setAuthor({ name: `${club.name} ${club.tag}`, iconURL: `https://cdn-old.brawlify.com/club/${club.badgeId}.png`})
+
+        const membersField = miembrosplus.map((member) => {
+          const { name, trophies } = member
+          return `<:reseteo:1178100588114882652> \`${trophies}\` | \`${name}\``
+        }).join('\n')
+
+        const membersField2 = miembrosminus.map((member) => {
+          const { name, trophies } = member
+          return `<:reseteo:1178100588114882652> \`${trophies}\` | \`${name}\``
+        }).join('\n')
+    
+        embed.addFields(
+          { name: 'Miembros Altos', value: `${membersField}`, inline: true },
+          { name: 'Miembros Bajos', value: `${membersField2}`, inline: true }
+        )
+
+        interaction.reply({ embeds: [embed], components: [boton, boton2]})
+      }
+
+      //interaction.reply({ embeds: [embed]})
   } 
 }
